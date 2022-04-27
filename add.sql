87baf8d345fea531a1006cebcf01a1c2f3159fd7
Annmariyaclarine <95731201+Annmariyaclarine@users.noreply.github.com>
2022-04-27 15:00:23 +0530

    Add files via upload

---
+++ add.sql
@@ -0,0 +1,18 @@
+create database ann;
+use ann;
+(use ann)
+DELIMITER //
+drop procedure if exists addnum //
+create procedure addnum()
+begin
+declare a int;
+declare b int;
+declare c int;
+set a=10;
+set b=20;
+set c=a+b;
+select concat('sum=',c);
+END //
+DELIMITER ;
+call addnum();
+
